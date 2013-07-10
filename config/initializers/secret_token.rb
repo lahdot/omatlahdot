aac550c76b47086187eda2ae3c0816ab2514e9fd
Jarmo Isotalo <jamo@isotalo.fi>
2013-07-10 19:28:49 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omatlahdot::Application.config.secret_token = '0123fb6949061ecae8ac0b09493c6af6e41c2df99a9de25fca7e92ac2f00c3e60fc09192e85c90506c14780d4132c348c9c34948a7975daf6e52215ba9d08b5f'
